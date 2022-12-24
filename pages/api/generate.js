e52f9ecec29ca72eac469539bc3ef400a7aed151
Joshua Omobola <omobolathejoshua@gmail.com>
2022-12-24 20:28:13 +0000

    Update generate.js and index.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,8 +5,27 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix = `
-Write a personal, cool and fun christmas short story greeting and appreciation from Joshua based on the description I give you. Use fun, short, emotional, colloquial words. Do not promise or pledge in the output. Just Write a heartful message to make them smile
+// const basePromptPrefix = `
+// Write a personal, cool and fun christmas short story greeting and appreciation from  based on the description I give you. Use fun, short, emotional, colloquial words. Do not promise or pledge in the output. Just Write a heartful message to make them smile
+
+// Description: Jude, loves shoes and gaming adventures. Lives in kigali
+
+// Output: Dear Jude,
+
+// Merry Christmas and a Happy New Year! I hope these holidays fill your heart with joy and laughter.
+
+// I know how much you love shoes and gaming adventures and wanted to thank you for all the times you shared your collection with me. I appreciate it and am grateful for your friendship.
+
+// I hope your Christmas is as wonderful as you are and that 2023 brings you all the good things you deserve. Have a magical holiday season, my friend.
+
+// Love,
+// Joshua
+
+// Description: 
+// `;
+const generateAction = async (req, res) => {
+  let basePromptPrefix = `
+Write a personal, cool and fun christmas short story greeting and appreciation from "${req.body.userName}" based on the description I give you. Use fun, short, emotional, colloquial words. Do not promise or pledge in the output. Just Write a heartful message to make them smile
 
 Description: Jude, loves shoes and gaming adventures. Lives in kigali
 
@@ -23,7 +42,9 @@ Joshua
 
 Description: 
 `;
-const generateAction = async (req, res) => {
+  
+  
+  
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
