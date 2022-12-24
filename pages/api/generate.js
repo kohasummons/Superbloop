6d4c71091e4a91a447d039735e557dd0a285e306
Joshua Omobola <omobolathejoshua@gmail.com>
2022-12-24 20:12:23 +0000

    bloop.

---
+++ pages/api/generate.js
@@ -0,0 +1,42 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = `
+Write a personal, cool and fun christmas short story greeting and appreciation from Joshua based on the description I give you. Use fun, short, emotional, colloquial words. Do not promise or pledge in the output. Just Write a heartful message to make them smile
+
+Description: Jude, loves shoes and gaming adventures. Lives in kigali
+
+Output: Dear Jude,
+
+Merry Christmas and a Happy New Year! I hope these holidays fill your heart with joy and laughter.
+
+I know how much you love shoes and gaming adventures and wanted to thank you for all the times you shared your collection with me. I appreciate it and am grateful for your friendship.
+
+I hope your Christmas is as wonderful as you are and that 2023 brings you all the good things you deserve. Have a magical holiday season, my friend.
+
+Love,
+Joshua
+
+Description: 
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\noutput:\n`,
+    temperature: 0.85,
+    max_tokens: 550,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,118 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import kohaImage from "../assets/koha.jpg";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const [userName, setUserName] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    if(userName === "") {
+      setIsGenerating(false);
+      return
+    }
+
+    console.log("Calling OpenAI...");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+  
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+  
+  const onUserName = (event) => {
+    setUserName(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Bloop - Send fun, merry greetings faster that you can spell jack  </title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Bloop</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>
+              Bloop is gonna help you write the sweetest christmas greetings
+              to the ones you love
+            </h2>
           </div>
         </div>
+        <div className="prompt-container">
+          <input 
+            className="name-box"
+            onChange={onUserName}
+            placeholder="What's your name?" 
+          />
+          <textarea
+            value={userInput}
+            onChange={onUserChangedText}
+            placeholder="Tell me something special about him/her"
+            className="prompt-box"
+          />
+          <div className="prompt-buttons">
+  <a
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+    </div>
+  </a>
+</div>
+           {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
       </div>
-      <div className="badge-container grow">
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+        </div>
+      </div>
+      {/* <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="https://twitter.com/kohawithstuff"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <Image src={kohaImage} alt="koha logo" />
+            <p>koha.gg</p>
           </div>
         </a>
-      </div>
+      </div> */}
+      <div className="footer">
+        <p>Love, <a
+          href="https://twitter.com/kohawithstuff"
+          target="_blank"
+          rel="noreferrer">Koha🌵</a> and Jide</p></div>
     </div>
   );
 };
